ff37f05f16eda4c70be0369714d4eac4dd32fae2
SimonaIstrate <simonaistrate.3112@gmail.com>
2019-01-25 17:54:00 +0200

    "Spring mvc project"

---
+++ src/main/java/com/example/springtutorialsmvc/RoomController.java
@@ -0,0 +1,32 @@
+package com.example.springtutorialsmvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class RoomController {
+    private static List<Room> rooms = new ArrayList<>();
+
+    static {
+        for(int i=0; i<10 ;i++){
+            rooms.add(new Room(i, "Room", "abc", "desc room"));
+        }
+    }
+
+    @RequestMapping("/rooms")
+    public String getAllRooms(Model model){
+        model.addAttribute("rooms", rooms);
+        return "rooms";
+    }
+
+    @RequestMapping("/hotel")
+    public String hotel(){
+        return "Hotels";
+    }
+
+}
---
+++ src/main/java/com/example/springtutorialsmvc/SpringTutorialsMvcApplication.java
@@ -0,0 +1,14 @@
+package com.example.springtutorialsmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTutorialsMvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringTutorialsMvcApplication.class, args);
+    }
+
+}
+
